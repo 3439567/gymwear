77ff2ca0c351a41106c4d6ca7094883dc7404cb2
Mangesh Waghmare <mangeshwaghmare619@gmail.com>
2026-06-26 19:17:51 +0530

    Updated API URL to Render

--- frontend/src/pages/Products.js
+++ frontend/src/pages/Products.js
@@ -7,7 +7,7 @@ function Products() {
   const { addToCart } = useContext(CartContext);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/products')
+    axios.get('https://gymwear-shop.onrender.com/api/products')
       .then(res => setProducts(res.data))
       .catch(err => console.error("Error fetching products:", err));
   }, []);
